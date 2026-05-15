405155fb84c3e48f1ec182a8c046766ca5d118b8
Houssem KALLEL <houssem.kallel@gmail.com>
2026-05-15 23:36:19 +0100

    feat: add functional AuthGuard redirecting unauthenticated users to /login

---
+++ src/app/core/auth/auth.guard.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing'
+import { Router } from '@angular/router'
+import { RouterTestingModule } from '@angular/router/testing'
+import { AuthService } from './auth.service'
+import { authGuard } from './auth.guard'
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router'
+
+describe('authGuard', () => {
+  let authServiceSpy: jasmine.SpyObj<AuthService>
+  let router: Router
+
+  beforeEach(() => {
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['isAuthenticated'])
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [{ provide: AuthService, useValue: authServiceSpy }]
+    })
+    router = TestBed.inject(Router)
+  })
+
+  it('returns true when authenticated', () => {
+    authServiceSpy.isAuthenticated.and.returnValue(true)
+    const result = TestBed.runInInjectionContext(() =>
+      authGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot)
+    )
+    expect(result).toBeTrue()
+  })
+
+  it('returns UrlTree to /login when not authenticated', () => {
+    authServiceSpy.isAuthenticated.and.returnValue(false)
+    const result = TestBed.runInInjectionContext(() =>
+      authGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot)
+    )
+    expect(result).toEqual(router.createUrlTree(['/login']))
+  })
+})
