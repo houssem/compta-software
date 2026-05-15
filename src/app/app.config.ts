9e2e0d3794de7521ce7776054297153208dec85d
Houssem KALLEL <houssem.kallel@gmail.com>
2026-05-15 23:37:04 +0100

    feat: configure Angular providers (http, echarts, animations) and dev proxy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core'
+import { provideRouter } from '@angular/router'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideEcharts } from 'ngx-echarts'
+import { routes } from './app.routes'
+import { jwtInterceptor } from './core/auth/jwt.interceptor'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
-};
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([jwtInterceptor])),
+    provideAnimations(),
+    provideEcharts(),
+  ]
+}
