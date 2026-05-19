8d99ea68ee246c0da4de761529e0b7e1dbb3f044
Houssem KALLEL <houssem.kallel@gmail.com>
2026-05-19 23:18:42 +0100

    feat(i18n): register TranslateModule in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,16 @@
-import { ApplicationConfig } from '@angular/core'
+import { ApplicationConfig, importProvidersFrom } from '@angular/core'
 import { provideRouter } from '@angular/router'
-import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { provideHttpClient, withInterceptors, HttpClient } from '@angular/common/http'
 import { provideAnimations } from '@angular/platform-browser/animations'
 import { provideEcharts } from 'ngx-echarts'
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 import { routes } from './app.routes'
 import { jwtInterceptor } from './core/auth/jwt.interceptor'
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json')
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +18,15 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([jwtInterceptor])),
     provideAnimations(),
     provideEcharts(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'fr',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    )
   ]
 }
