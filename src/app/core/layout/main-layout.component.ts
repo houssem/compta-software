ff2aad02ab17b9dcf8340e9834609693b1a9c264
Houssem KALLEL <houssem.kallel@gmail.com>
2026-05-19 23:20:12 +0100

    feat(i18n): translate sidebar navigation

--- src/app/core/layout/main-layout.component.ts
+++ src/app/core/layout/main-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'
+import { RouterLink, RouterLinkActive } from '@angular/router'
+import { TranslateModule } from '@ngx-translate/core'
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, TranslateModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
