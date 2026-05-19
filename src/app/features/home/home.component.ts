f87aefb0ae672e6df9ae49f8525d3bac77747236
Houssem KALLEL <houssem.kallel@gmail.com>
2026-05-19 23:21:20 +0100

    feat(i18n): translate home/landing page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core'
-import { RouterLink } from '@angular/router'
+import { Component, OnInit, inject } from '@angular/core'
+import { Router, RouterLink } from '@angular/router'
+import { TranslateModule } from '@ngx-translate/core'
+import { AuthService } from '../../core/auth/auth.service'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, TranslateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  private auth = inject(AuthService)
+  private router = inject(Router)
+
+  ngOnInit() {
+    if (this.auth.currentUser() !== null) {
+      this.router.navigate(['/dashboard-vente'])
+    }
+  }
+}
